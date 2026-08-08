ad9e981c140a55d69729255a821d0480c25ff6fd
kai <bpurdy12@icloud.com>
2026-08-08 16:27:42 -0700

    basic path implementation

--- source/Paths.hx
+++ source/Paths.hx
@@ -1,2 +1,31 @@
 package;
 
+import sys.FileSystem;
+
+class Paths {
+	public static var CUR_MOD:String;
+	public static var ASSETS:String = 'assets/';
+	public static var MODS:String = 'mods/';
+
+	public static function getPath(file:String):String
+	{
+		var path = '';
+		if (CUR_MOD != null)
+		{
+			path = '$MODS$CUR_MOD/$file';
+			if (!FileSystem.exists(path))
+			{
+				path = '$ASSETS$path';
+				return path;
+			}
+		}
+		else
+			return '$ASSETS$path';
+	}
+
+	inline public static function getImage(file:String)
+		return getPath('images/$file.png');
+
+	inline public static function getScript(file:String)
+		return getPath('scripts/$file.hx');
+}
