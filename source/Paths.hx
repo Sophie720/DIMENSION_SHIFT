fc65cca864ea1127b87ba09e877e18bb59df86ba
kai <bpurdy12@icloud.com>
2026-08-09 17:38:05 -0700

    remove CUR_MOD, not needed anymore lol

--- source/Paths.hx
+++ source/Paths.hx
@@ -4,7 +4,6 @@ import sys.FileSystem;
 import sys.io.File;
 
 class Paths {
-	public static var CUR_MOD:String = 'test-mod';
 	inline public static final ASSETS:String = './assets/';
 	inline public static final MODS:String = './mods/';
 	inline public static final MODS_LIST:String = './mods.list';
